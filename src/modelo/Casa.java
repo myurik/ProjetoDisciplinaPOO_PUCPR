f5e4b793fc1c4a5d527668d9f29363179b2975e3
Matheus Yuri Miguel <msyf.business@gmail.com>
2026-05-03 00:51:58 -0300

    (feat): Novos Atributos adicionados na classe Casa (areaConstruida e tamanhoTerreno) e adicionado um novo Override do método mostrarDadosFinanc na classe Casa.

--- src/modelo/Casa.java
+++ src/modelo/Casa.java
@@ -1,12 +1,26 @@
 package modelo;
 
 public class Casa extends Financiamento{
-    public Casa(double valorImovel, double taxaJurosAnual, int prazoFinanciamentoAnos){
+    private final double areaConstruida;
+    private final double tamanhoTerreno;
+
+    public Casa(double valorImovel, double taxaJurosAnual, int prazoFinanciamentoAnos, double areaConstruida, double tamanhoTerreno) {
         super(valorImovel, taxaJurosAnual, prazoFinanciamentoAnos);
+        this.areaConstruida = areaConstruida;
+        this.tamanhoTerreno = tamanhoTerreno;
     }
 
     public double calcularPagamentoMensal(){
         double taxaSeguro = 80;
         return super.calcularPagamentoMensal() + taxaSeguro;
     }
+
+    @Override
+    public void mostrarDadosFinanc() {
+        System.out.println("---- Dados do Financiamento: Casa ----");
+        super.mostrarDadosFinanc(); // Chama a impressão genérica da classe mãe
+        System.out.println("Área Construída: " + this.areaConstruida + " m²");
+        System.out.println("Tamanho do Terreno: " + this.tamanhoTerreno + " m²");
+    }
+}
 }
