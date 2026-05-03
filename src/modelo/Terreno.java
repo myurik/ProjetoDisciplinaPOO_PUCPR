5ceb54b0bfa341bdf23bfec4d08e3ddba9f8bef4
Matheus Yuri Miguel <msyf.business@gmail.com>
2026-05-03 00:59:04 -0300

    (feat): Novo Atributo adicionado na classe Terreno (tipoZona) e adicionado um novo Override do método mostrarDadosFinanc na classe Terreno.

--- src/modelo/Terreno.java
+++ src/modelo/Terreno.java
@@ -1,11 +1,21 @@
 package modelo;
 
 public class Terreno extends Financiamento{
-    public Terreno(double valorImovel, double taxaJurosAnual, int prazoFinanciamentoAnos){
+    private final String tipoZona;
+
+    public Terreno(double valorImovel, double taxaJurosAnual, int prazoFinanciamentoAnos, String tipoZona) {
         super(valorImovel, taxaJurosAnual, prazoFinanciamentoAnos);
+        this.tipoZona = tipoZona;
     }
 
     public double calcularPagamentoMensal(){
         return super.calcularPagamentoMensal() * 1.02;
+    }
+
+    @Override
+    public void mostrarDadosFinanc() {
+        System.out.println("---- Dados do Financiamento: Terreno ----");
+        super.mostrarDadosFinanc();
+        System.out.println("Tipo de Zona: " + this.tipoZona);
     }
 }
